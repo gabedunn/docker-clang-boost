309b7925d560e64cd20083a0b16c9192f75b2ab9
Gabe Dunn <gdunn@pipewise.ca>
2021-05-31 09:27:18 -0600

    write build script

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env sh
 
 main () {
-	echo "Hello, World!"
+	# config variables
+	_REPO="clang-boost"
+	_DISTROS=("ubuntu" "opensuse")
+
+	# loop through the selected distros and build their images
+	for _DISTRO in "${_DISTROS[@]}"; do
+		# generate the command used to build the image
+		_BUILD_CMD="docker build $_DISTRO -t $_REPO:$_DISTRO"
+
+		# output the status
+		echo "Building: $_DISTRO ($_BUILD_CMD)"
+
+		# run the build command
+		$_BUILD_CMD
+	done
 }
 
 main
